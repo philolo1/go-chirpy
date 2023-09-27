2533819c0363d168b6acdbe63f00d63b441a9ffb
Patrick Klitzke <philologos14@gmail.com>
2023-09-27 23:14:24 +0900

    Current progress

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func middlewareCors(next http.Handler) http.Handler {
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	corsMux := middlewareCors(mux)
 
 	server := http.Server{
