787c252cfc99b0cab8d7e62324bfd6785f15c65c
Patrick Klitzke <philologos14@gmail.com>
2023-09-27 23:32:10 +0900

    More server functionality

--- main.go
+++ main.go
@@ -18,9 +18,16 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthz)
 	corsMux := middlewareCors(mux)
 
 	server := http.Server{
