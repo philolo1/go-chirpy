c97dd8cc184fe7ed74e081ff5df87c6263bce797
Patrick Klitzke <philologos14@gmail.com>
2023-09-28 00:39:56 +0900

    Chapter 1 complete

--- main.go
+++ main.go
@@ -5,8 +5,34 @@ import (
 	http "net/http"
 )
 
+type ApiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *ApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits += 1
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *ApiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	text := fmt.Sprintf("Hits: %v", cfg.fileserverHits)
+	w.Write([]byte(text))
+}
+
+func (cfg *ApiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
+	w.Write([]byte("OK"))
+}
+
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request: %v\n", r.URL)
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
@@ -25,9 +51,12 @@ func healthz(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	apiConfig := ApiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/", http.StripPrefix("/app", apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/metrics", apiConfig.metrics)
+	mux.HandleFunc("/reset", apiConfig.reset)
 	corsMux := middlewareCors(mux)
 
 	server := http.Server{
