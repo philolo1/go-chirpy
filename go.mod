93f575d9f323b7d387e31bac9126d7b12a12b1a3
Patrick Klitzke <philologos14@gmail.com>
2023-09-28 16:10:43 +0900

    Current Progress chapter 3

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/philolo1/go-chirpy
 
 go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	http "net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type ApiConfig struct {
@@ -17,10 +19,18 @@ func (cfg *ApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *ApiConfig) metrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	text := fmt.Sprintf("Hits: %v", cfg.fileserverHits)
-	w.Write([]byte(text))
+	w.Write([]byte(fmt.Sprintf(`
+<html>
+
+<body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>
+`, cfg.fileserverHits)))
 }
 
 func (cfg *ApiConfig) reset(w http.ResponseWriter, r *http.Request) {
@@ -50,14 +60,37 @@ func healthz(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func apiRouter(apiConfig *ApiConfig) http.Handler {
+	r := chi.NewRouter()
+
+	r.HandleFunc("/reset", apiConfig.reset)
+
+	r.Get("/healthz", healthz)
+
+	return r
+}
+
+func adminRouter(apiConfig *ApiConfig) http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/metrics", apiConfig.metrics)
+
+	return r
+}
+
 func main() {
 	apiConfig := ApiConfig{}
-	mux := http.NewServeMux()
-	mux.Handle("/", http.StripPrefix("/app", apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/healthz", healthz)
-	mux.HandleFunc("/metrics", apiConfig.metrics)
-	mux.HandleFunc("/reset", apiConfig.reset)
-	corsMux := middlewareCors(mux)
+
+	r := chi.NewRouter()
+	fsHandler := http.StripPrefix("/app", apiConfig.middlewareMetricsInc(http.FileServer(http.Dir("."))))
+
+	r.Handle("/app", fsHandler)
+	r.Handle("/app/*", fsHandler)
+
+	r.Mount("/api", apiRouter(&apiConfig))
+	r.Mount("/admin", adminRouter(&apiConfig))
+
+	corsMux := middlewareCors(r)
 
 	server := http.Server{
 		Handler: corsMux,
